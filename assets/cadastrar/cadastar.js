9dc29ae3972b70d7cfc22e55883d1e781e81fdf1
garototojai <jaysonjjs@gmail.com>
2023-10-18 22:34:44 -0300

    dia 18

--- assets/cadastrar/cadastar.js
+++ assets/cadastrar/cadastar.js
@@ -5,13 +5,17 @@ let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
 
 
     const tarefa = {
+        id: Date.now(), 
                      titulo : document.querySelector("#titulo").value ,
                      descricao: document.querySelector("#descricao").value,
-                     pontos: document.querySelector("#pontos").value
-}
+                     pontos: document.querySelector("#pontos").value,
+
+                  concluida: false
+    }
+
  tarefas.push(tarefa)
 
-
+    
 
 localStorage.setItem("tarefas",JSON.stringify(tarefas))
 
